575882727dda7e8b8aa3541c13ed4258a9cd6ad4
fjferrada <fjferrada@outlook.com>
2024-12-11 23:41:03 -0300

    Update scrappRIO.py

--- scrappRIO.py
+++ scrappRIO.py
@@ -54,13 +54,15 @@ while True:
             mime='text/csv',
         )
     
-    while True:
-        hora_actual = dt.datetime.now()
-        url = f'https://www.coordinador.cl/wp-admin/admin-ajax.php?action=export_energia_csv&fecha_inicio={hora_actual.strftime("%Y-%m-%d")}&fecha_termino={hora_actual.strftime("%Y-%m-%d")}&hora_inicio=00:00:00&hora_termino=23:59:59'
-        scraper = cloudscraper.create_scraper()
-        response = scraper.get(
-            url
-            )
+    # while True:
+    hora_actual = dt.datetime.now()
+    url = f'https://www.coordinador.cl/wp-admin/admin-ajax.php?action=export_energia_csv&fecha_inicio={hora_actual.strftime("%Y-%m-%d")}&fecha_termino={hora_actual.strftime("%Y-%m-%d")}&hora_inicio=00:00:00&hora_termino=23:59:59'
+    scraper = cloudscraper.create_scraper()
+    print("scrapper listo")
+    response = scraper.get(
+        url
+        )
+    print("scrapper terminado")
         # response.content
         # pd.read_csv(StringIO(response.content.decode('utf-8-sig')))
         # Based on the error message, it seems the CSV data has an inconsistent number of columns
@@ -69,84 +71,84 @@ while True:
         # Print the raw content to inspect it
     
         # Try reading with error_bad_lines=False to skip problematic rows
-        try:
-            rio = pd.read_csv(StringIO(response.content.decode('utf-8-sig')), 
-                        sep=';',
-                        on_bad_lines='skip',
-                        skiprows=4)["FECHA;HORA;BCMG P.AZUCAR_22O".split(";")] # For newer pandas versions
-        except Exception as e:
-            print("no captura")
-            print(response.content.decode('utf-8-sig'))
-            time.sleep(30)
-            continue
-    
-        if dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 0, 0, 0) < hora_actual <= dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 8, 0, 0):
-            bloque = 0
-        elif dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 8, 0, 0) < hora_actual <= dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 18, 0, 0):
-            bloque = 1
-        elif dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 18, 0, 0) < hora_actual <= dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 23, 0, 0)+dt.timedelta(hours=1):
-            bloque = 2
-        url = f"https://www.coordinador.cl/wp-content/uploads/{hora_actual.year}/{str(hora_actual.month).zfill(2)}/PROGRAMA{hora_actual.strftime('%Y%m%d')}.zip"
-        # Download the zip file
-        response = scraper.get(url)
-        zip_file = zipfile.ZipFile(io.BytesIO(response.content))
-    
-        # Find the xlsx file containing "PO" in the name
-        xlsx_file = [f for f in zip_file.namelist() if "PO" in f][0]
-    
-        # Read the specific sheet with selected columns
-        cols_to_use = [[3,4],
-                    [7,8],
-                    [11,12]]
-        dt.datetime.now()
-        df = pd.read_excel(
-                    zip_file.open(xlsx_file),
-                    sheet_name="TCO",
-                    usecols=[i-1 for i in cols_to_use[bloque]],
-                    skiprows=6
-                )
-        df.columns = "BCMG P.AZUCAR_22O;cmg".split(";")
-        df = pd.concat(
-            [
-                df,
-                pd.DataFrame([["ERNC",0]], columns = "BCMG P.AZUCAR_22O;cmg".split(";")) 
-            ],
-            ignore_index=True
-        ).dropna().drop_duplicates()
-        aux = rio.drop_duplicates().merge(df, on = "BCMG P.AZUCAR_22O", how = "left").assign(
-            datetime = lambda data: pd.to_datetime(data.FECHA+" "+data.HORA, format='%Y-%m-%d %H:%M:%S')
-        ).dropna().drop_duplicates().reset_index(drop =True)
-        ultimo_dato = aux.datetime.max()
-        if ultimo_dato not in pd.to_datetime(RIO.datetime).to_list():
-            all_dates_in_rio = RIO.datetime.unique()
-            aux_not_in_RIO = aux.query("~datetime.isin(@all_dates_in_rio)")
-            if len(aux_not_in_RIO) > 0:
-                RIO = pd.concat([RIO, aux_not_in_RIO], ignore_index=True).sort_values(by = "datetime").reset_index(drop = True)
-                RIO.to_csv("RIOs/registro.csv", index = False)
-        filtro_fecha = dt.datetime.now() - dt.timedelta(hours = 48)
-        for i in RIO.columns:
-            #if len(RIO) > 48:
-            st.session_state.data[i] = RIO.query("datetime >= @filtro_fecha")[i].to_list()
-        filtroFecha = dt.datetime.now()-dt.timedelta(hours = 48)
-        fig = px.line(x = st.session_state.data["datetime"], y = st.session_state.data["cmg"]
-                      , hover_data = {"price_maker": st.session_state.data["BCMG P.AZUCAR_22O"]}
-                      , title = "Real-time RIO"
-                      , labels = {"datetime": "Fecha y Hora", "cmg": "USD/MWh Barra Nueva Pan de Azucar"})
-        fig.update_layout(
-            xaxis=dict(
-                title=dict(
-                    text="time"
-                )
-            ),
-            yaxis=dict(
-                title=dict(
-                    text="USD/MWh"
-                )
+    try:
+        rio = pd.read_csv(StringIO(response.content.decode('utf-8-sig')), 
+                    sep=';',
+                    on_bad_lines='skip',
+                    skiprows=4)["FECHA;HORA;BCMG P.AZUCAR_22O".split(";")] # For newer pandas versions
+    except Exception as e:
+        print("no captura")
+        print(response.content.decode('utf-8-sig'))
+        time.sleep(30)
+        continue
+
+    if dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 0, 0, 0) < hora_actual <= dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 8, 0, 0):
+        bloque = 0
+    elif dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 8, 0, 0) < hora_actual <= dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 18, 0, 0):
+        bloque = 1
+    elif dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 18, 0, 0) < hora_actual <= dt.datetime(hora_actual.year, hora_actual.month, hora_actual.day, 23, 0, 0)+dt.timedelta(hours=1):
+        bloque = 2
+    url = f"https://www.coordinador.cl/wp-content/uploads/{hora_actual.year}/{str(hora_actual.month).zfill(2)}/PROGRAMA{hora_actual.strftime('%Y%m%d')}.zip"
+    # Download the zip file
+    response = scraper.get(url)
+    zip_file = zipfile.ZipFile(io.BytesIO(response.content))
+
+    # Find the xlsx file containing "PO" in the name
+    xlsx_file = [f for f in zip_file.namelist() if "PO" in f][0]
+
+    # Read the specific sheet with selected columns
+    cols_to_use = [[3,4],
+                [7,8],
+                [11,12]]
+    dt.datetime.now()
+    df = pd.read_excel(
+                zip_file.open(xlsx_file),
+                sheet_name="TCO",
+                usecols=[i-1 for i in cols_to_use[bloque]],
+                skiprows=6
+            )
+    df.columns = "BCMG P.AZUCAR_22O;cmg".split(";")
+    df = pd.concat(
+        [
+            df,
+            pd.DataFrame([["ERNC",0]], columns = "BCMG P.AZUCAR_22O;cmg".split(";")) 
+        ],
+        ignore_index=True
+    ).dropna().drop_duplicates()
+    aux = rio.drop_duplicates().merge(df, on = "BCMG P.AZUCAR_22O", how = "left").assign(
+        datetime = lambda data: pd.to_datetime(data.FECHA+" "+data.HORA, format='%Y-%m-%d %H:%M:%S')
+    ).dropna().drop_duplicates().reset_index(drop =True)
+    ultimo_dato = aux.datetime.max()
+    if ultimo_dato not in pd.to_datetime(RIO.datetime).to_list():
+        all_dates_in_rio = RIO.datetime.unique()
+        aux_not_in_RIO = aux.query("~datetime.isin(@all_dates_in_rio)")
+        if len(aux_not_in_RIO) > 0:
+            RIO = pd.concat([RIO, aux_not_in_RIO], ignore_index=True).sort_values(by = "datetime").reset_index(drop = True)
+            RIO.to_csv("RIOs/registro.csv", index = False)
+    filtro_fecha = dt.datetime.now() - dt.timedelta(hours = 48)
+    for i in RIO.columns:
+        #if len(RIO) > 48:
+        st.session_state.data[i] = RIO.query("datetime >= @filtro_fecha")[i].to_list()
+    filtroFecha = dt.datetime.now()-dt.timedelta(hours = 48)
+    fig = px.line(x = st.session_state.data["datetime"], y = st.session_state.data["cmg"]
+                  , hover_data = {"price_maker": st.session_state.data["BCMG P.AZUCAR_22O"]}
+                  , title = "Real-time RIO"
+                  , labels = {"datetime": "Fecha y Hora", "cmg": "USD/MWh Barra Nueva Pan de Azucar"})
+    fig.update_layout(
+        xaxis=dict(
+            title=dict(
+                text="time"
+            )
+        ),
+        yaxis=dict(
+            title=dict(
+                text="USD/MWh"
             )
         )
-        with plot_spot:
-            st.plotly_chart(fig, use_container_width = True, key = f"key_{n_n}")
-        n_n+=1
-        # st.line_chart(x = st.session_state.data["datetime"], y = st.session_state.data["cmg"])
-        time.sleep(60)
+    )
+    with plot_spot:
+        st.plotly_chart(fig, use_container_width = True, key = f"key_{n_n}")
+    n_n+=1
+    # st.line_chart(x = st.session_state.data["datetime"], y = st.session_state.data["cmg"])
+    time.sleep(60)
         # plot_placeholder.plotly_chart(fig, use_container_width=True, key = "plot_1")
